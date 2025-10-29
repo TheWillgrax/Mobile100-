e4fc7e281aadd9585c23662b78c28f87611ca72a
TheWillgrax <145234634+TheWillgrax@users.noreply.github.com>
2025-10-29 11:59:57 -0600

    fix productos
    fix

--- services/api.js
+++ services/api.js
@@ -46,7 +46,7 @@ const expoConfigToken =
   null;
 
 const baseURL = process.env.EXPO_PUBLIC_API_URL || expoConfigBaseURL || fallbackBaseURL;
-const defaultToken = process.env.EXPO_PUBLIC_API_TOKEN || expoConfigToken || null;
+const defaultToken = 'b29b84da47aa04c04863c9ec9657880de85929c9f95ac1619893a195ae8e038c3a7fcbcf6eb90da30c2f3453bef68c50e8d1149f3cdb9a8f09382e5d6e883294f22a79f0e911802dba7f060f16f1848834e6fde808b10438ef2b591c389a83e299368b769d06dd381e4970bda1f4b5f5c038f9a2274e0ab3c14b82c928b833b4';
 
 export const api = axios.create({
   baseURL,
@@ -57,7 +57,10 @@ if (defaultToken) {
   api.defaults.headers.common.Authorization = `Bearer ${defaultToken}`;
 }
 
+console.log('Token:' + defaultToken);
+
 export const setApiAuthToken = (token) => {
+  console.log('Token SET:' + token);
   if (token) {
     api.defaults.headers.common.Authorization = `Bearer ${token}`;
   } else {
@@ -68,6 +71,7 @@ export const setApiAuthToken = (token) => {
 api.interceptors.request.use(async (config = {}) => {
   try {
     const token = await storage.get("token");
+    console.log('Token RE:' + token);
     if (token) {
       config.headers = config.headers ?? {};
       if (!config.headers.Authorization) {
