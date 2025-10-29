391567ea17fb23150dfe3f1d141e9629b113e80c
TheWillgrax <145234634+TheWillgrax@users.noreply.github.com>
2025-10-29 00:29:51 -0600

    Embed default authorizer token

--- services/api.js
+++ services/api.js
@@ -11,7 +11,7 @@ const fallbackBaseURL =
 const authorizerToken =
   process.env.EXPO_PUBLIC_API_AUTHORIZER ??
   process.env.EXPO_PUBLIC_API_AUTHORIZER_TOKEN ??
-  null;
+  "b29b84da47aa04c04863c9ec9657880de85929c9f95ac1619893a195ae8e038c3a7fcbcf6eb90da30c2f3453bef68c50e8d1149f3cdb9a8f09382e5d6e883294f22a79f0e911802dba7f060f16f1848834e6fde808b10438ef2b591c389a83e299368b769d06dd381e4970bda1f4b5f5c038f9a2274e0ab3c14b82c928b833b4";
 
 export const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL || fallbackBaseURL,
